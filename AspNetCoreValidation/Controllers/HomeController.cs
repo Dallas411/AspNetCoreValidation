16ebc3f362bd1d4450cd7411188208f230f1bdda
Marco Dalla Libera <marco@dallalibera.net>
2019-02-07 17:19:49 +0100

    Aggiungere i file di progetto.

---
+++ AspNetCoreValidation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreValidation.Models;
+
+namespace AspNetCoreValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View(new IndexViewModel());
+        //}
+
+        public IActionResult Index(IndexViewModel indexViewModel)
+        {
+            ViewBag.ElaborationTime = DateTime.Now;
+            ViewBag.ModelStateIsValid = ModelState.IsValid;
+            return View(new IndexViewModel());
+        }
+    }
+}
---
+++ AspNetCoreValidation/Models/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreValidation.Models
+{
+    public class IndexViewModel
+    {
+        [Required(ErrorMessage = "Il Nome è obbligatorio")]
+        [StringLength(5,ErrorMessage ="Lunghezza massima 5 caratteri")]
+        public string Name { get; set; }
+        [RegularExpression(@"^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$", ErrorMessage = "Formato gg/MM/yyyy")]
+        public string DoB { get; set; }
+    }
+}
